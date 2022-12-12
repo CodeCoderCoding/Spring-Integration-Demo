f406103360da4ea0c6e58a60fb70931c1db8ca5c
CodeCoderCoding <kneil5778@gmail.com>
2022-12-12 16:14:09 +0800

    add project "transformer-string"

---
+++ endpoints/transformer/transformer-string/src/main/java/com/supremepole/transformerstring/TransformerStringDemo.java
@@ -0,0 +1,31 @@
+package com.supremepole.transformerstring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.support.MessageBuilder;
+
+public class TransformerStringDemo {
+
+    private ApplicationContext ctx = null;
+    private MessageChannel inChannel = null;
+
+    public TransformerStringDemo() {
+        ctx = new ClassPathXmlApplicationContext("transformer-string.xml");
+        inChannel = ctx.getBean("in-channel",MessageChannel.class);
+    }
+
+    private void publish() {
+        Person person=new Person();
+        Message<Person> tradeMsg = MessageBuilder.withPayload(person).build();
+        inChannel.send(tradeMsg, 10000);
+        System.out.println("Message published.");
+    }
+
+    public static void main(String[] args) {
+        TransformerStringDemo demo = new TransformerStringDemo();
+        demo.publish();
+    }
+
+}

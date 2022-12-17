b23e740edc22900c2060dc74245a7ae55ce2f5fb
CodeCoderCoding <kneil5778@gmail.com>
2022-12-17 19:22:40 +0800

    update project “transformer-xml”

--- components-transformer/transformer-xml/src/main/java/com/supremepole/transformerxml/TransformerXMLDemo.java
+++ components-transformer/transformer-xml/src/main/java/com/supremepole/transformerxml/TransformerXMLDemo.java
@@ -5,24 +5,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.support.MessageBuilder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 
 public class TransformerXMLDemo {
 
     private ApplicationContext ctx = null;
     private MessageChannel inChannel = null;
+    private String path="./components-transformer/transformer-xml/src/main/resources/";
 
     public TransformerXMLDemo() {
         ctx = new ClassPathXmlApplicationContext("transformer-xml.xml");
-        inChannel = ctx.getBean("trades-in-channel",MessageChannel.class);
+        inChannel = ctx.getBean("out-channel",MessageChannel.class);
     }
 
-    private void publish() {
-        Message<String> tradeMsg = MessageBuilder.withPayload("<trade status='NEW' account='B12D45' direction='BUY'/>").build();
+    private void publish() throws IOException, SAXException, ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(path+"XMLFile.xml");
+        Message<?> tradeMsg = MessageBuilder.withPayload(document).build();
         inChannel.send(tradeMsg, 10000);
         System.out.println("Message published.");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         TransformerXMLDemo demo = new TransformerXMLDemo();
         demo.publish();
     }
